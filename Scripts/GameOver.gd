9a15d2f42f2e0a4a31d795c78764f752685d7296
V3L35 <moridin1011@hotmail.com>
2020-07-05 14:33:48 +0200

    V1.0 Release

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -2,7 +2,10 @@ extends Node
 
 
 func _on_YES_pressed():
-	get_tree().change_scene("res://Scenes/Arena.tscn")
+	if Global.points >= 2020*Global.num_level:
+		get_tree().change_scene("res://Scenes/Arena_boss.tscn")
+	else:
+		get_tree().change_scene("res://Scenes/Arena.tscn")
 
 
 func _on_NO_pressed():
--- Scripts/MagicFlame.gd
+++ Scripts/MagicFlame.gd
@@ -1,8 +1,5 @@
 extends AnimatedSprite
 
-func _ready():
-	pass
-
 
 func _on_AnimatedSprite_animation_finished():
 	queue_free()
